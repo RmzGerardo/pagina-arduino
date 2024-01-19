e4b832a608fc33c0c5f45ae2997a4e94da9408ef
Gerardo Ramirez <61709895+RmzGerardo@users.noreply.github.com>
2024-01-19 17:48:51 -0600

    ahora si final

--- js.js
+++ js.js
@@ -1,7 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
+
+
+$(window).on('load', function() {
+  setInterval(function() {
+      $("head title").fadeOut(300, function() {
+          $(this).text($(this).text().substring(1) + $(this).text().substring(0, 1))
+              .fadeIn(300);
+      });
+  }, 300);
+
+  let progressBar = $('.progress-bar');
+  let progressValue = 0;
+  let interval = setInterval(increaseProgress,
+      50);
+
+  function increaseProgress() {
+      progressValue += 1;
+      progressBar.css('width', progressValue + '%').attr('aria-valuenow', progressValue);
+      $('.persentase').text(progressValue + '%');
+      if (progressValue >= 100) {
+          clearInterval(interval);
+          $(".preloader").fadeOut("slow");
+      }
+  }
+})
 
 
 
@@ -29,26 +50,6 @@ $(document).ready(function(){
 
 
 
-$(window).on('load', function() {
-  setInterval(function() {
-      $("head title").fadeOut(300, function() {
-          $(this).text($(this).text().substring(1) + $(this).text().substring(0, 1))
-              .fadeIn(300);
-      });
-  }, 300);
 
-  let progressBar = $('.progress-bar');
-  let progressValue = 0;
-  let interval = setInterval(increaseProgress,
-      50);
 
-  function increaseProgress() {
-      progressValue += 1;
-      progressBar.css('width', progressValue + '%').attr('aria-valuenow', progressValue);
-      $('.persentase').text(progressValue + '%');
-      if (progressValue >= 100) {
-          clearInterval(interval);
-          $(".preloader").fadeOut("slow");
-      }
-  }
-})
+
